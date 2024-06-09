4f507d96b6cdb91645cf6949aa8fd56350f8e744
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2024-06-09 20:25:52 +0700

    FORMAT CODE!
    Format code using camelCase, rename variables to describe their use

--- source_code/trie.h
+++ source_code/trie.h
@@ -2,47 +2,44 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
-struct trienode
+struct trieNode
 {
-	bool wordexisted;
-	trienode** next;
-	vector<pair<string, string>> definintions;
-	trienode(bool here)
+	bool wordExisted;
+	trieNode** next;
+	std::vector<std::pair<std::string, std::string>> definintions;
+	trieNode(bool wordExisted)
 	{
-		wordexisted = here;
-		next = new trienode * [69]; // ASCII from 32 to 64 and from 91 to 126
+		this -> wordExisted = wordExisted;
+		next = new trieNode* [69]; // ASCII from 32 to 64 and from 91 to 126
 		for (int i = 0; i < 69; i++) next[i] = nullptr;
 	}
 };
 
 void load()
 {
-	//load a csv line to c++ whatever thing
+	
 }
 
-void insert(trienode*& t, string word)
+void insert(trieNode*& pCurrent, std::string word)
 {
-	if (t == nullptr)
+	if (pCurrent == nullptr)
 	{
-		if (word.length() == 0) t = new trienode(true);
+		if (word.length() == 0) pCurrent = new trieNode(true);
 		else
 		{
-			t = new trienode(false);
-			insert(t->next[word[0] - 97], word.erase(0, 1));
+			pCurrent = new trieNode(false);
+			insert(pCurrent->next[word[0] - 97], word.erase(0, 1));
 		}
 	}
 	else
 	{
-		if (word.length() == 0) t->wordexisted = true;
-		else insert(t->next[word[0] - 97], word.erase(0, 1));
+		if (word.length() == 0) pCurrent->wordExisted = true;
+		else insert(pCurrent->next[word[0] - 97], word.erase(0, 1));
 	}
 }
-
-bool find(trienode* t, string word)
+bool find(trieNode* pCurrent, std::string word)
 {
-	if (!t) return false;
-	if (word.length() == 0) return t->wordexisted;
-	else return find(t->next[word[0] - 97], word.erase(0, 1));
+	if (!pCurrent) return false;
+	if (word.length() == 0) return pCurrent->wordExisted;
+	else return find(pCurrent->next[word[0] - 97], word.erase(0, 1));
 }
