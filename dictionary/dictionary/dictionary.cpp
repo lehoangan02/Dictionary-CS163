88ec4b62f51c3e546a610f140921c33bf75ca306
refute2change <madguyinthehouse@gmail.com>
2024-06-10 12:26:32 +0700

    sfml added, not in use yet. Also go rewrite the find function, 3 new indexes neef their own case

---
+++ dictionary/dictionary/dictionary.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include "trie.h"
+
+using namespace std;
+
+int main()
+{
+	trieNode* trie = nullptr;
+	string word = "";
+	while (1)
+	{
+		getline(cin, word);
+		if (word.length() == 0) break;
+		for (int i = 0; i < word.length(); i++)
+		{
+			if (word[i] >= 65 && word[i] < 91) word[i] += 32;
+		}
+		insert(trie, word);
+	}
+	cout << find(trie, "hello");
+	return 0;
+}
+
+//uihfjdshfiu
---
+++ dictionary/dictionary/trie.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+#include <vector>
+
+struct trieNode
+{
+	bool wordExisted;
+	trieNode** next;
+	std::vector<std::pair<std::string, std::string>> definitions;
+	trieNode(bool wordExisted)
+	{
+		this->wordExisted = wordExisted;
+		next = new trieNode * [29];
+		// 0->25: letters
+		// 26: space (' ')
+		// 27: dash ('-')
+		// 28: apostrophe (')
+
+		for (int i = 0; i < 29; i++) next[i] = nullptr;
+	}
+};
+
+void load();
+
+void insert(trieNode*& pCurrent, std::string word);
+
+bool find(trieNode* pCurrent, std::string word);
