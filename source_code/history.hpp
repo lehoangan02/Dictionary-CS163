3c4aa3f88d4e511a796968b86bc5417afbaef7f4
DPTPhatUS <23125065@student.hcmus.edu.vn>
2024-07-05 13:05:02 +0700

    merging

--- source_code/history.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <fstream>
-#include <vector>
-#include <string>
-void readHistory(std::vector<std::string>& history);
-void writeHistory(std::string newHistory); 
--- source_code/removeQuotationMarkDuplicate.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <string>
-
-void removeQuotationMarkDuplicate(std::string& inputString);
