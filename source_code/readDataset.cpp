4021f45571075f92c038da49a983c1f4715c181e
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2024-07-07 15:17:32 +0700

    fixing/revert damage

--- source_code/readDataset.cpp
+++ source_code/readDataset.cpp
@@ -6,6 +6,11 @@ bool readDatasetCSV(std::string filename, trieNode*& pRoot)
 {
     std::ifstream inputStream;
     inputStream.open(filename);
+    if (!inputStream.is_open())
+    {
+        printf("[DEBUG] - file not found!\n");
+        return false;
+    }
     std::string line = "";
     getline(inputStream, line);
     std::vector<std::pair<std::string, std::string>> definitionVec;
@@ -43,7 +48,7 @@ bool readDatasetCSV(std::string filename, trieNode*& pRoot)
         if (word != previousWord && previousWord != "")
         {
             std::cout << "[DEBUG] end of vec\n";
-            //insert(pRoot, previousWord, definitionVec);
+            insert(pRoot, previousWord, definitionVec);
             for (int i = 0; i < definitionVec.size(); ++i)
             {
                 // std::cout << "[DEBUG]" << previousWord << " ";
@@ -69,9 +74,7 @@ bool readDatasetCSV(std::string filename, trieNode*& pRoot)
         //     std::cout << definitionVec[i].first << " ";
         //     std::cout << definitionVec[i].second << std::endl;
         // }
-        
-        
-        //insert(pRoot, previousWord, definitionVec);
+        insert(pRoot, previousWord, definitionVec);
     }
     return true;
 }
