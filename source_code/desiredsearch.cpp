a6cbf31294d0ea7a96c41979ceb61a9a49e3d7db
refute2change <madguyinthehouse@gmail.com>
2024-07-24 10:06:33 +0700

    suggestion search updated, integration with search button next

--- source_code/desiredsearch.cpp
+++ source_code/desiredsearch.cpp
@@ -2,12 +2,54 @@
 
 desiredsearch::desiredsearch()
 {
+	topdefault.loadFromFile("assets/image/TopResultNone.png");
+	tophover.loadFromFile("assets/image/TopResultHover.png");
+	middefault.loadFromFile("assets/image/MiddleResultNone.png");
+	midhover.loadFromFile("assets/image/MiddleResultHover.png");
+	bottomdefault.loadFromFile("assets/image/BottomResultNone.png");
+	bottomhover.loadFromFile("assets/image/BottomResultHover.png");
+	solodefault.loadFromFile("assets/image/SoloResultNone.png");
+	solohover.loadFromFile("assets/image/SoloResultHover.png");
+	f.loadFromFile("assets/fomt/SourceSans3-VariableFont_wght.ttf");
 	available = 0;
 	for (int i = 0; i < 3; i++)
 	{
-		options.push_back(Button());
+		options.push_back(Button(middefault, midhover, f, "", 24));
 	}
 }
+
+void desiredsearch::adjustsuggestions()
+{
+	options.clear();
+	switch (available)
+	{
+		case 0:
+		{
+			break;
+		}
+		case 1:
+		{
+			options[0] = Button(solodefault, solohover, f, categories[0], 24);
+			break;
+		}
+		case 2:
+		{
+			options[0] = Button(topdefault, tophover, f, categories[0], 24);
+			options[1] = Button(bottomdefault, bottomhover, f, categories[1], 24);
+			break;
+		}
+		case 3:
+		{
+			options[0] = Button(topdefault, tophover, f, categories[0], 24);
+			options[1] = Button(middefault, midhover, f, categories[1], 24);
+			options[2] = Button(bottomdefault, bottomhover, f, categories[2], 24);
+			break;
+		}
+	}
+	for (int i = 0; i < options.size(); ++i) options[i].setPosition(sf::Vector2u(125, 145 + 65 * i));
+}
+
+
 
 void desiredsearch::updateoptions(std::string word, trieNode* pRoot)
 {
@@ -20,11 +62,11 @@ void desiredsearch::updateoptions(std::string word, trieNode* pRoot)
 		categories.resize(3);
 		count = 3;
 	}
-
-	for (int i = 0; i < options.size(); ++i) options[i].setPosition(sf::Vector2u(125, 145 + 65 * i));
+	available = count;
+	adjustsuggestions();
 }
 
-void desiredsearch::draw()
+void desiredsearch::draw(sf::RenderWindow &w)
 {
-
+	for (Button b: options) b.draw(w);
 }
--- source_code/desiredsearch.h
+++ source_code/desiredsearch.h
@@ -8,8 +8,12 @@ class desiredsearch
 {
 	std::vector<std::string> categories;
 	std::vector<Button> options;
+	sf::Texture topdefault, middefault, bottomdefault, solodefault, tophover, midhover, bottomhover, solohover;
+	sf::Font f;
+	sf::Texture hover;
 	int available;
 	desiredsearch();
 	void updateoptions(std::string word, trieNode* pRoot);
-	void draw();
+	void adjustsuggestions();
+	void draw(sf::RenderWindow &w);
 };
