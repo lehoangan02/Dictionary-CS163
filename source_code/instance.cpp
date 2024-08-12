0090b516379f4c19a82972304e0d862bd5e35323
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2024-08-12 14:44:01 +0700

    fix opted importing
    -remove quotation marks from OPTED dataset
    -fix auto-save variable loading in page 1
    -
    
    Co-Authored-By: Do Phan Tuan Phat <161719217+DPTPhatUS@users.noreply.github.com>

--- source_code/instance.cpp
+++ source_code/instance.cpp
@@ -384,8 +384,15 @@ void instance::operatePage1()
 	if (!loadedSave)
 	{
 		deleteWholeTrie(pRoot);
+		if (!loadAutoSave)
+		{
+			loadAutoSaveSetting();
+			loadAutoSave = true;
+		}
 		if (autoSave)
+		{
 			deserializeBinaryWrapper(pRoot, word4Def);
+		}
 		readFavourite(pRootFavourite);
 		pCurrentFavourite = pRootFavourite;
 		loadedSave = true;
